1654fb8a81c3d7a432c8bb1470e6fbec756f5c4e
XuChao <xuchao@guestin.cn>
2025-09-19 09:25:03 +0800

    redis add exposed GenerateKey func

--- redis/config.go
+++ redis/config.go
@@ -1,5 +1,12 @@
 package redis
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/guestin/mob/merrors"
+)
+
 const (
 	ModuleName       = "redis"
 	DefaultPort      = 6379
@@ -18,10 +25,16 @@ type Config struct {
 	DbIdx     int    `toml:"db" validate:"omitempty,gte=0,lte=128" mapstructure:"db"`
 	Password  string `toml:"password" mapstructure:"password"`
 	KeyPrefix string `toml:"keyPrefix" validate:"omitempty,excludesall= !@#$%^&*()\t\n\r" mapstructure:"keyPrefix"`
+	KeySP     string `toml:"keySP" validate:"omitempty,excludesall= !@#$%^&*()\t\r\n" mapstructure:"keySP"`
 }
 
 var _cfg *Config
 
-func GetConfig() Config {
-	return *_cfg
+func GenerateKey(parts ...string) string {
+	merrors.Assert(len(parts) > 0, "key parts is required")
+	prefix := _cfg.KeyPrefix
+	if !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
+	return fmt.Sprintf("%s%s", prefix, strings.Join(parts, "/"))
 }
